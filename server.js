c7baf15054af13b07ab06622460df94399ed5a1b
= <=>
2026-06-29 13:37:28 +0200

    feat: add support for additional CORS origins in environment configuration

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     credentials: true,
-    origin:[process.env.CLIENT_URL || "http://localhost:5173", "https://therabridge.vercel.app"],
+    origin:[process.env.CLIENT_URL || "http://localhost:5173", ...(process.env.CLIENT_URL_2 ? [process.env.CLIENT_URL_2] : [])],
   })
 );
 app.use(express.json());
