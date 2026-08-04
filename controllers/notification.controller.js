bd60e27b815dd186c7c0bdd54b5313f292c21145
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-04 17:51:35 +0200

    refactor: move createNotification function to notification service

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -85,20 +85,4 @@ export const deleteAllNotifications = async (req, res) => {
   }
 };
 
-export const createNotification = async (recipientId, type, title, body, data = {}, senderId = null) => {
-  try {
-    const notification = new Notification({
-      recipient: recipientId,
-      sender: senderId,
-      type,
-      title,
-      body,
-      data,
-    });
-    await notification.save();
-    return notification;
-  } catch (error) {
-    console.error("Failed to create notification:", error.message);
-    return null;
-  }
-};
+export { createNotification } from "../services/notification.service.js";
