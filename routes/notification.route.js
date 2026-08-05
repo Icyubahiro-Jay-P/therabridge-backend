4f7d7cc241e6f70bcd7768b2b5166ac733e3e9b1
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-05 02:45:39 +0200

    feat: add jsonBody middleware with a 10kb limit to notification routes

--- routes/notification.route.js
+++ routes/notification.route.js
@@ -8,9 +8,11 @@ import {
   deleteAllNotifications,
 } from "../controllers/notification.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+import { jsonBody } from "../middleware/jsonBody.js";
 
 const router = express.Router();
 
+router.use(jsonBody("10kb"));
 router.use(authMiddleware);
 
 router.get("/", getMyNotifications);
