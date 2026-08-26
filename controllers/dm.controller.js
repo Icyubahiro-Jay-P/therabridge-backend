3a5d4da55a0b0960fbc807c3337bae5925f1dee8
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-26 13:36:47 +0200

    fix: remove unused imports from dm.controller.js

--- controllers/dm.controller.js
+++ controllers/dm.controller.js
@@ -1,24 +1,3 @@
-import { Message } from "../models/chat.model.js";
-import User from "../models/user.model.js";
-import Notification from "../models/notification.model.js";
-import { emitToUser } from "../sockets/chatSocket.js";
-import { awardMessagePoints, MESSAGE_POINTS } from "../utils/points.js";
-import { withTransaction } from "../utils/transactions.js";
-import { createNotification } from "../services/notification.service.js";
-import {
-  getPaginationParams,
-  formatPaginatedResponse,
-  getCursorPaginationParams,
-  formatCursorPaginatedResponse,
-} from "../utils/pagination.js";
-import { encryptField, decryptField } from "../utils/crypto.js";
-import {
-  decryptMessageContent,
-  LONG_POLL_INTERVAL_MS,
-  LONG_POLL_TIMEOUT_MS,
-  INITIAL_CATCHUP_WINDOW_MS,
-} from "./chat.utils.js";
-
 export {
   sendMessage,
   editMessage,
