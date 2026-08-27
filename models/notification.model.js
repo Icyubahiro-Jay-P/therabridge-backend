3dbea390e6c52ef181ebf4929af032f34570946b
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-27 18:53:42 +0200

    feat: remove billing type from notification schema

--- models/notification.model.js
+++ models/notification.model.js
@@ -14,7 +14,7 @@ const notificationSchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      enum: ["message", "community_invite", "exercise_reminder", "system", "mood_reminder", "crisis_alert", "community_update", "streak_milestone", "mood_checkin", "session_booked", "session_cancelled", "session_reminder", "billing"],
+      enum: ["message", "community_invite", "exercise_reminder", "system", "mood_reminder", "crisis_alert", "community_update", "streak_milestone", "mood_checkin", "session_booked", "session_cancelled", "session_reminder"],
       required: true,
     },
     title: {
